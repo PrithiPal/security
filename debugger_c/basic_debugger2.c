68aaeef8299e3588604e9e21615e2bbd75e9dd0c
Prithi <pprithip@sfu.ca>
2020-03-28 16:19:57 -0700

    Added ptrace examples for 32 bits.

---
+++ debugger_c/basic_debugger2.c
@@ -0,0 +1,81 @@
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/reg.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/syscall.h>   /* For SYS_write etc */
+
+
+int main()
+{   
+    
+    pid_t child;
+    long orig_eax, eax, orig_ebx,orig_ecx,orig_edx;
+    long params[3];
+    int status;
+    int insyscall = 0;
+    child = fork();
+
+    // child process : TRACEE
+    if(child == 0) {
+        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+        execl("/bin/ls", "ls", NULL);
+    }
+    
+    // parent process : TRACER
+    else {
+       while(1) {
+
+          wait(&status);
+          if(WIFEXITED(status))
+              break;
+          orig_eax = ptrace(PTRACE_PEEKUSER,
+                     child, 8 * ORIG_RAX, NULL);
+
+            /*
+          orig_ebx = ptrace(PTRACE_PEEKUSER,child,4*ORIG_RBX,NULL);
+          orig_ecx = ptrace(PTRACE_PEEKUSER,child,4*ORIG_RCX,NULL);
+          orig_edx = ptrace(PTRACE_PEEKUSER,child,4*ORIG_RDX,NULL);
+            */
+
+
+          printf("%ld %ld %ld %ld\n",orig_eax,orig_ebx,orig_ecx,orig_edx);
+          
+          if(orig_eax == SYS_write) {
+
+
+              printf("inside\n");
+             if(insyscall == 0) {
+                /* Syscall entry */
+                insyscall = 1;
+                params[0] = ptrace(PTRACE_PEEKUSER,
+                                   child, 4 * RBX,
+                                   NULL);
+                params[1] = ptrace(PTRACE_PEEKUSER,
+                                   child, 4 * RCX,
+                                   NULL);
+                params[2] = ptrace(PTRACE_PEEKUSER,
+                                   child, 4 * RDX,
+                                   NULL);
+                printf("Write called with "
+                       "%ld, %ld, %ld\n",
+                       params[0], params[1],
+                       params[2]);
+                }
+          else { /* Syscall exit */
+                eax = ptrace(PTRACE_PEEKUSER,
+                             child, 4 * RAX, NULL);
+                    printf("Write returned "
+                           "with %ld\n", eax);
+                    insyscall = 0;
+                }
+            }
+            ptrace(PTRACE_SYSCALL,
+                   child, NULL, NULL);
+
+
+        }
+    }
+    return 0;
+}
